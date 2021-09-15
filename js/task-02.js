d5f70a66d6644bba91ad709bbf9a9455ef3ff39f
Yevhenii-bs <evgen.bs@gmail.com>
2021-09-15 11:16:45 +0300

    create hw

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
+// после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+
+ingredients.map((el) => {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(el));
+  document.getElementById("ingredients").appendChild(li)
+});
